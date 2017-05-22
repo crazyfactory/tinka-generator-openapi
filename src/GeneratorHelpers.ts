05e72aad0584679a8342f55a3d5e215d6e2b3468
Pai <gavinsitti@hotmail.com>
2017-05-22 17:33:58 +0700

    wip: getAllControllers

--- src/GeneratorHelpers.ts
+++ src/GeneratorHelpers.ts
@@ -1,15 +1,25 @@
-import {IApiMethod, IController} from "./interfaces";
+import {IApiMethod, IController, IPathsData} from "./interfaces";
 
 export class GeneratorHelpers {
-  public static extractControllersInfo(data): IController[] {
+  public static extractControllersInfo(paths: IPathsData): IController[] {
 
   }
 
-  public static getAllMethods(data): IApiMethod[] {
-
+  public static getAllControllers(paths: IPathsData): IController[] {
+    let controllers: IController[] = [];
+    for (let url in paths) {
+      for (let httpmethod in paths[url]) {
+        paths[url][httpmethod].tags.map((className) => {
+          controllers.push({
+            name: className,
+            methods: []
+          })
+        })
+      }
+    }
   }
 
-  public static getAllClasses(data): IApiMethod[] {
+  public static getAllMethods(paths: IPathsData): IApiMethod[] {
 
   }
 }
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -3,19 +3,19 @@ export interface IGenerator {
 }
 
 export interface IController {
-  controllerName: string;
+  name: string;
   methods: IApiMethod[];
 }
 
 export interface IApiMethod {
   name: string;
+  classNames: string[]; // meta data in order to match its class(es) later
   returnType: string;
   httpMethod: "GET" | "POST" | "PUT" | "DELETE";
   methodParams: IParams[];
   queryParams: IParams[];
   bodyParams: IParams[];
   url: string;
-  className: string; // meta data in order to match its class later
 }
 
 export interface IParams {
@@ -23,7 +23,7 @@ export interface IParams {
   type: string;
   in?: string;
   description?: string;
-  required: true;
+  required?: true;
 }
 
 export interface IPathsData {
